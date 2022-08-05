58129d6289958b3ebebd391492466f12c1200074
haniemiresmaeili <haniemeer80@gmail.com>
2022-08-05 16:45:39 +0430

    js 3

--- app.js
+++ app.js
@@ -59,4 +59,27 @@ function formatDate(date) {
   function getCurrentLocation(event) {
     event.preventDefault();
     navigator.geolocation.getCurrentPosition(searchLocation);
-  }  
+  }
+  // function convertToFahrenheit(event) {
+//   event.preventDefault();
+//   let temperatureElement = document.querySelector("#temperature");
+//   temperatureElement.innerHTML = 66;
+// }
+
+// function convertToCelsius(event) {
+//   event.preventDefault();
+//   let temperatureElement = document.querySelector("#temperature");
+//   temperatureElement.innerHTML = 19;
+// }
+
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
+
+let searchForm = document.querySelector("#search_btn");
+searchForm.addEventListener("click", handleSubmit);
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+// searchCity("Tehran");  
