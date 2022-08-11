165963906f6b610ca4aa026b7deb421b791d6f37
haniemiresmaeili <haniemeer80@gmail.com>
2022-08-11 14:55:47 +0430

    added forecast

--- app.js
+++ app.js
@@ -29,7 +29,35 @@ function formatDay(timestamp) {
   return days[day];
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
 
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+ 
+}
 
 
 function displayTemperature(response) {
@@ -78,3 +106,4 @@ form.addEventListener("submit", handleSubmit);
 // let celciousLink=document.querySelector("#celcious-link")
 // celciousLink.addEventListener("click",celcious)
 search("Tehran");
+displayForecast();
